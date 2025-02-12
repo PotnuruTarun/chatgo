3028681e3029cc69b5c689b70d7c0527dd07cce1
PotnuruTarun <tarunpotnuru12@gmail.com>
2025-02-12 18:57:30 +0530

    deploying...

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js";
-import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
 
-import connectToMongoDB from "./db/connectToMongoDB.js";
+import connectToMongo from "./db/connectToMongo.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
@@ -30,6 +30,6 @@ app.get("*", (req, res) => {
 });
 
 server.listen(PORT, () => {
-	connectToMongoDB();
+	connectToMongo();
 	console.log(`Server Running on port ${PORT}`);
 });
